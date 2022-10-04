7fc8f687f4af3a830ab764e4065a00f960016921
ashwinigujjar31 <113108021+ashwinigujjar31@users.noreply.github.com>
2022-10-04 10:28:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,4 @@
-// Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
+public class helloworld {
+public static void main(String[] args){
+System.out.println("Hello World");
+}}
